e8411feb6d47b584e889b3d2477b65a47f6319e7
xiran sun <xiransun@xirandeMacBook-Air.local>
2017-08-06 21:04:25 +0800

    add reference

---
+++ 参考/myflow-master/extract/flow/cavas.js
@@ -0,0 +1,18 @@
+var canvas = {
+    canvas: {
+        //线上面点的队列
+        lp: [],
+        init: function () {
+            $('.flow').width($(window).width()).height($(window).height());
+            var p = Raphael($('.flow')[0], $('.flow').width(), $('.flow').height());
+            flow.canvas.can = p;
+
+
+        }
+    }
+}
+
+function init() {
+    this.canvas.init();
+    this.main.init();
+}
