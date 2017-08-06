aa1f545ed4d74b3d7ea580dcd6ec7ac953b495b9
xiran sun <xiransun@xirandeMacBook-Air.local>
2017-08-06 20:51:24 +0800

    没有droppable就没有dropped

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
     console.log("my script!");
-    $('.dragaable').eq(2).draggable({ scroll: true });
+    $('.dragaable').eq(2).draggable({
+        scroll: true,
+        revert: function(dropped) {
+            console.log('execute revert function!');
+            if (dropped) {
+                console.log('has dropped!');
+                return false;
+            }
+            console.log('has not dropped!');
+            return true;
+        }
+    });
+
+    $('#platte').droppable();
 });
