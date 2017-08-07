38cdf0cf4760352967768fdceee5e4b8166ee2eb
sunxiran <maricle01@163.com>
2017-08-07 08:39:33 +0800

    histoory 8.7

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     console.log("my script!");
+<<<<<<< Updated upstream
     $('.dragaable').eq(2).draggable({
         scroll: true,
         revert: function(dropped) {
@@ -14,4 +15,38 @@ $(document).ready(function () {
     });
 
     $('#platte').droppable();
+=======
+
+    $('.dragaable').eq(2).draggable({
+        scroll: true,
+        cursor: "pointer",
+        create: function () {
+            console.log('draggable creation!');
+        },
+
+        drag: function (event, ui) {
+            $("#dashboard").html("top: " + ui.position.top + "; left: " + ui.position.left);
+        },
+
+        start: function (event, ui) {
+            // console.log('drag start!');
+        },
+
+        stop: function (event, ui) {
+            // console.log('drag stop!');
+
+        },
+        revert: function (dropped) {
+            let result = true;
+            if ($(dropped).hasClass('droppable')) {
+                result = false;
+            }
+            return result;
+        },
+    });
+
+    $('.droppable').droppable({
+
+    });
+>>>>>>> Stashed changes
 });
