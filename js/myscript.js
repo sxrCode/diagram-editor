78408e628b3f72e0ed9fd8e61daf165a327b26bb
sunxiran <maricle01@163.com>
2017-08-08 10:16:14 +0800

    primary oop

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,53 @@
+function widgetFactory(type) {
+    if (type === 'ellipse') {
+        return new EliipseWidget();
+    } else if (type === 'rectangle') {
+        return new RectangleWidget();
+    } else {
+        return new CircleWidget();
+    }
+}
+
+class EliipseWidget {
+    constructor() {
+        this.widget = $('<div class="dragaable ellipse"></div>');
+    }
+
+    createFigure() {
+        $(this.widget).draggable({
+            scroll: true,
+            cursor: 'pointer',
+            start: this.onStartDrag,
+            revert: this.onRevert,
+        });
+
+        return this.widget;
+    }
+
+    onStartDrag(event, ui) {
+        console.log('drag start!');
+        if (ui) {
+            $(ui.helper).css({ 'border-color': 'red' });
+        }
+    }
+
+    onRevert(dropped) {
+        let result = true;
+        if ($(dropped).hasClass('droppable')) {
+            result = false;
+        }
+        return result;
+    }
+}
+
+
 $(document).ready(function () {
     console.log("my script!");
+    let ellipse = widgetFactory('ellipse');
+    $('#left-closet').append(ellipse.createFigure());
 
+    /*
     $('.dragaable').eq(2).draggable({
-        scroll: true,
-        cursor: "pointer",
-        //helper: 'clone',
-
         create: function () {
             console.log('draggable creation!');
         },
@@ -24,20 +66,15 @@ $(document).ready(function () {
 
         stop: function (event, ui) {
             // console.log('drag stop!');
-
         },
-        revert: function (dropped) {
-            let result = true;
-            if ($(dropped).hasClass('droppable')) {
-                result = false;
-            }
-            return result;
-        },
+        revert: function (dropped) {},
     });
+    */
 
     $('.droppable').droppable({
-        drop: function(event, ui) {
+        drop: function (event, ui) {
             console.log('drop!!');
         },
     });
 });
+
